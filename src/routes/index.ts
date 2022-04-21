e75947482cf3b6df45db9288588ad65abe76a697
Achionyinye <achionyinyegiftbarbie@gmail.com>
2022-04-21 18:10:59 +0100

    second push

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,92 +1,93 @@
-import express from  'express';
-import { body, validationResult,param } from 'express-validator';
+import express from 'express';
+import { body, validationResult, param } from 'express-validator';
 import fetchData from '../model/helpers';
 import createAccount from '../services/createAccount';
 import transferFunds from '../services/transferFunds';
 import fetchServices from '../services/fetchServices';
 
-const router =  express.Router();
-router.post('/create-account', 
-// body('account').isInt()
-// .withMessage("account must be a number")
-// .isLength({min: 10, max: 10})
-// .withMessage("account must have exactly 10 digits"),
-// body('balance').isFloat().withMessage("Balance must be a number"),
-function(req: express.Request, res: express.Response) {
-  // check for validation error(s)
-  // const errors = validationResult(req);
+const router = express.Router();
+router.post('/create-account',
+  // body('account').isInt()
+  // .withMessage("account must be a number")
+  // .isLength({min: 10, max: 10})
+  // .withMessage("account must have exactly 10 digits"),
+  // body('balance').isFloat().withMessage("Balance must be a number"),
+  async function (req: express.Request, res: express.Response) {
+    // check for validation error(s)
+    // const errors = validationResult(req);
     // if (!errors.isEmpty()) {
     //   return res.status(400).json({ errors: errors.array() });
     // }
 
-  let accountDetail: Balance = req.body;  
-  const result  = createAccount(accountDetail);
+    let accountDetail: Balance = req.body;
+    const result = await createAccount(accountDetail);
+   console.log("result",result);
 
     // if(!result){
     //  res.json({message:"Account number already exist"});
     //  return;
     // }
-    // res.json({ balances: result });
+    res.json({ balance: result });
   });
 
-router.post('/transfer', 
-body('from').isInt()
-.withMessage("account must be a number")
-.isLength({min: 10, max: 10})
-.withMessage("account must have exactly 10 digits"),
-body('to').isInt()
-.withMessage("account must be a number")
-.isLength({min: 10, max: 10})
-.withMessage("account must have exactly 10 digits"),
-body('amount').isFloat().withMessage("Amount must be a number"),
-body('transferDescription').isString().withMessage("Transfer description must be a string"),
-function(req: express.Request, res: express.Response, next: express.NextFunction) {
-   // check for validation error(s)
-   const errors = validationResult(req);
-   if (!errors.isEmpty()) {
-     return res.status(400).json({ errors: errors.array() });
-   }
+router.post('/transfer',
+  body('from').isInt()
+    .withMessage("account must be a number")
+    .isLength({ min: 10, max: 10 })
+    .withMessage("account must have exactly 10 digits"),
+  body('to').isInt()
+    .withMessage("account must be a number")
+    .isLength({ min: 10, max: 10 })
+    .withMessage("account must have exactly 10 digits"),
+  body('amount').isFloat().withMessage("Amount must be a number"),
+  body('transferDescription').isString().withMessage("Transfer description must be a string"),
+  function (req: express.Request, res: express.Response, next: express.NextFunction) {
+    // check for validation error(s)
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     let transferInfo: TransferInfo = req.body;
-    const result  = transferFunds(transferInfo);
+    const result = transferFunds(transferInfo);
     const key = Object.keys(result)[0];
-    switch(key){
+    switch (key) {
       case 'missingSenderMessage':
-        res.json({message:"Sender account number does not exist"});
+        res.json({ message: "Sender account number does not exist" });
         break;
       case 'missingReceiverMessage':
-        res.json({message:"Receiver account number does not exist"});
+        res.json({ message: "Receiver account number does not exist" });
         break;
       case 'insufficientMessage':
-        res.json({message:"Insufficient funds"});
+        res.json({ message: "Insufficient funds" });
         break;
-      default: 
-      res.json({ transaction: result });
+      default:
+        res.json({ transaction: result });
     }
   });
 
-router.get('/balance', function(req: express.Request, res: express.Response, next: express.NextFunction) {
-    res.json({ balances: fetchServices.fetchBalances() });
-  });
+router.get('/balance', function (req: express.Request, res: express.Response, next: express.NextFunction) {
+  res.json({ balances: fetchServices.fetchBalances() });
+});
 
-router.get('/transactions', function(req: express.Request, res: express.Response, next: express.NextFunction) {
-    const data: DataBase = fetchData;
-    res.json({ transactions: data?.transactions });
-  });
+router.get('/transactions', function (req: express.Request, res: express.Response, next: express.NextFunction) {
+  const data: DataBase = fetchData;
+  res.json({ transactions: data?.transactions });
+});
 
-router.get('/balance/:accountNumber', 
-param('accountNumber').isInt()
-  .withMessage("accountNumber must be a number")
-  .isLength({min: 10, max: 10})
-  .withMessage("Account number must have exactly 10 digits"),
-function(req: express.Request, res: express.Response, next: express.NextFunction) {
-   // check for validation error(s)
-   const errors = validationResult(req);
-   if (!errors.isEmpty()) {
-     return res.status(400).json({ errors: errors.array() });
-   }
+router.get('/balance/:accountNumber',
+  param('accountNumber').isInt()
+    .withMessage("accountNumber must be a number")
+    .isLength({ min: 10, max: 10 })
+    .withMessage("Account number must have exactly 10 digits"),
+  function (req: express.Request, res: express.Response, next: express.NextFunction) {
+    // check for validation error(s)
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     const accountNumber: number = +(req?.params?.accountNumber);
     const result = fetchServices.fetchBalance(accountNumber);
-    switch(Boolean(result)) {
+    switch (Boolean(result)) {
       case true:
         res.json({ balance: result });
         break;
@@ -95,22 +96,22 @@ function(req: express.Request, res: express.Response, next: express.NextFunction
     }
   });
 
-  router.get('/bank-statement/:accountNumber',
+router.get('/bank-statement/:accountNumber',
   param('accountNumber').isInt()
-  .withMessage("accountNumber must be a number")
-  .isLength({min: 10, max: 10})
-  .withMessage("accountNumber must have exactly 10 digits"),
-   function(req: express.Request, res: express.Response, next: express.NextFunction) {
-  // check for validation error(s)
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
+    .withMessage("accountNumber must be a number")
+    .isLength({ min: 10, max: 10 })
+    .withMessage("accountNumber must have exactly 10 digits"),
+  function (req: express.Request, res: express.Response, next: express.NextFunction) {
+    // check for validation error(s)
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     const accountNumber: number = +(req?.params?.accountNumber);
     const result = fetchServices.fetchBankStatement(accountNumber);
-    
-    const key  = Object.keys(result[0])[0];
-    switch(key) {
+
+    const key = Object.keys(result[0])[0];
+    switch (key) {
       case 'wrongAccount':
         res.json({ message: "Account number does not exist" });
         break;
@@ -122,4 +123,4 @@ function(req: express.Request, res: express.Response, next: express.NextFunction
     }
   });
 
-  export default router;
+export default router;
--- src/services/createAccount.ts
+++ src/services/createAccount.ts
@@ -1,39 +1,14 @@
-import fs from 'fs';
-import path from 'path';
-import fetchData from '../model/helpers';
 import models from '../model/schemas';
-const createAccount = (accountData: Balance)=>{
-    // create database.json if it does not exist
-  // try {
-  //   // fs.stat
-  //   fs.statSync(path.join(__dirname,path.sep,"..","/model/database.json"));
-  //        } 
-  // catch (error) {
-  //         fs.writeFile(path.join(__dirname,path.sep,"..","/model/database.json"),JSON.stringify({"transactions":[], "balances":[]}), function (err) {
-  //           if (err) throw err;
-  //           console.log('File is created successfully.');
-  //         })
-  //   }
-
+const createAccount = async(accountData: Balance)=>{
     let accountDetail: Balance  = accountData;
-    // accountDetail.createdAt = (new Date()).toISOString();
-    const data: DataBase = fetchData;
-    let existingAccounts =  models.BalanceModel.find({account: accountDetail.account});
-    // const alreadyExists: Balance | undefined = existingAccounts.find(individualAccount => individualAccount.account === accountDetail.account);
-    // if(alreadyExists){
-    // //  res.json({message:"Account number already exist"});
-    //  return null;
-    // }else{
-
-    //     // update existingAccounts
-    //     existingAccounts.push(accountDetail);
-    //     data.balances = existingAccounts;
-    
-    //     // update dataBase
-    //     fs.writeFileSync(path.join(__dirname,path.sep,"..","/model/database.json"),JSON.stringify(data))
-    //     return existingAccounts;
-    // }
-    console.log(existingAccounts);
-    
+    let accountAlreadyExist = await models.BalanceModel.find({account : accountDetail.account});
+    if(accountAlreadyExist.length > 0){
+      return {message:"Account number already exist"};
+    }      
+    else{
+    let newAccount = new models.BalanceModel({...accountDetail});
+    let savedAccounts =  await newAccount.save();
+    return savedAccounts;
+    } 
 }
 export default createAccount;
